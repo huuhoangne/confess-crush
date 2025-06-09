4da85267c666d405a9ff3b730dff438856aeea19
huuhoangne <anhlaductrong164@gmail.com>
2025-06-10 00:10:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 iconst CONFIG = {
     titleWeb: "Helo Cục Cứk Của Hữu Hoàng Nè Hẹ Hẹ",
-    introTitle: 'Nhã Phương ',
+    introTitle: 'Nhã Phương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao mày cứ đi nhầm đường
     Lạc vào tim tao lẻ loi:))
     Đằng sau chữ yêu đây là thương hẹ hẹ mới lụm được mấy câu:)`,
-    btnIntro: '^^Hẹ Hẹ^^',
+    btnIntro: '^^HẹHẹ^^',
     title: 'Phải chăng tôi đã yêu bà🥰',
     desc: 'Tôi say hình dáng và khuôn mặt bà rất thưởng dê >>> ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tôi thương bà lắm á , đừng bỏ tôi nheeeer :<<<<< .',
+    reply: 'Yêu thì yêu mà yêu thì yêu mõi hữu hoàng cu te phô mai que<33333333',
+    mess: 'Mình biết mà 🥰. Yêu Hữu Hoàng Rất Rất Nhiều😘😘',
+    messDesc: 'Tôi thương bà lắm á , đừng bỏ tôi nheeee :<<<<< .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/huuhoangne'Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/share/1K1FVZLHaY/
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/share/1K1FVZLHaY/
 }
